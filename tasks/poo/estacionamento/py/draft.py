2abee98b495c9ce6686f5c876cfc628b4d304088
Hellys Fernandes <hellysfernandesrotimei@gmail.com>
2025-12-10 11:21:58 -0300

    inicialização da atividade estacionamneto

---
+++ tasks/poo/estacionamento/py/draft.py
@@ -0,0 +1,9 @@
+class Estacionamento:
+    def __init__(self):
+        self.__veicolos: list[Veicolo] = []
+        self.__horaAtual: int = 0 
+
+class Veicolo:
+    def __init__(self):
+        self.__id: str = ""
+        self
